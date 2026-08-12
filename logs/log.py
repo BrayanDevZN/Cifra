89867c15fc81534f322eb8bbea554a8f53b4f859
Brayan <lsouzadiasvaz@gmail.com>
2026-08-12 13:26:16 -0300

    fix:create logs

---
+++ logs/log.py
@@ -0,0 +1,18 @@
+"""
+configuração global de logs
+"""
+
+import logging
+
+# Configura o log para ir APENAS para o arquivo e limpa os handlers do terminal
+logging.basicConfig(
+    filename='backend.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    encoding='utf-8',
+    force=True  
+)
+
+
+#Variavel que carrega o log
+logger = logging.getLogger(__name__)
