a39c4b82d2e9c269602b0fc1e7a7765dbb068972
Brayan <lsouzadiasvaz@gmail.com>
2026-08-12 15:24:33 -0300

    fix: open ai llm

--- infra/core/settings.py
+++ infra/core/settings.py
@@ -8,20 +8,29 @@ class EnvError(Exception):
 
 try:
 
-    from pathlib import Path
     import os
-    from dotenv import load_dotenv
 
-    #Caminho do env
-    BASE_DIR = Path(__file__).resolve().parent
+    if os.path.exists("infra/core/.env"):
+        
 
-    #Carrega as variaveis de ambiente
-    if os.path.exists(BASE_DIR / ".env"):
-        load_dotenv(BASE_DIR / ".env")
+        from pathlib import Path
+        from dotenv import load_dotenv
+
+        #Caminho do env
+        BASE_DIR = Path(__file__).resolve().parent
+        
+
+        #Carrega as variaveis de ambiente
+        if os.path.exists(BASE_DIR / ".env"):
+            
+            load_dotenv(BASE_DIR / ".env")
 
 
     #Pega a url que faz conexão com banco de dados
-    url = os.getenv(".env")
+    url = os.getenv("url")
+
+    key = os.getenv("key")
+    
 
 except Exception as e:
 
