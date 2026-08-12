7c70ae85a367f6973341117b29c9aa4c1256e273
Brayan <lsouzadiasvaz@gmail.com>
2026-08-12 15:54:45 -0300

    fix: debug logs

--- logs/log.py
+++ logs/log.py
@@ -6,7 +6,7 @@ import logging
 
 # Configura o log para ir APENAS para o arquivo e limpa os handlers do terminal
 logging.basicConfig(
-    filename='backend.log',
+    filename='logs/app.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     encoding='utf-8',
